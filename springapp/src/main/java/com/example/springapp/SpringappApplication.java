741f57aff680ad3dedecfafeb9c70299128879de
Iamneo <techadmin@iamneo.ai>
2023-08-17 06:54:37 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -0,0 +1,9 @@
+package main.java.com.example.springapp;
+
+public class SpringappApplication {
+
+    public static void main(String[] args)
+    {
+        SpringappApplication.run(SpringApplication.class, args);
+    }
+}
